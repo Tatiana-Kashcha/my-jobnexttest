70211a8f910f7e6bb3f205296c7fe2770131d473
Tatiana Kashcha <tkashcha@gmail.com>
2025-04-01 17:56:21 +0300

    update

--- src/app/actions/fetchCandidates.ts
+++ src/app/actions/fetchCandidates.ts
@@ -1,8 +1,9 @@
 export async function fetchCandidates(queryString: string) {
-  const baseUrl = "http://localhost:3000";
+  // const baseUrl = "http://localhost:3000";
   // значення базового url має зберігатися в .env та отримуватися = process.env.BASE_URL,
   // щоб не створювати .env тут отримано локально;
 
+  const baseUrl = process.env.BASE_URL;
   const response = await fetch(`${baseUrl}/api/candidates?${queryString}`);
 
   if (!response.ok) {
